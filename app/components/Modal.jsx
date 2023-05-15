3563765f25e2cf8feec3503e104d54e78a815bd8
karan2668 <95574383+karan2668@users.noreply.github.com>
2023-05-15 08:53:41 +0530

    first commit

---
+++ app/components/Modal.jsx
@@ -0,0 +1,158 @@
+"use client";
+import React, { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
+const Modal = ({ setOpenModal, updateModal, Lists }) => {
+  updateModal = { ...updateModal };
+  // console.log(updateModal);
+  const [inputValue, setinputValue] = useState({
+    category: updateModal?.category || "",
+    task_title: updateModal?.task_title || "",
+    date_time: updateModal?.date_time || ""
+  });
+
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setinputValue((prevUser) => ({ ...prevUser, [name]: value }));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent the default form submission behavior
+
+    // check if the updateModal object exists and is truthy
+    if (updateModal?.update) {
+      let updatedLists = [...Lists]; // create a copy of the original array
+
+      // get the sub-array to update from the copy of the original array
+      const listToUpdate = updatedLists[updateModal?.index]?.filter(
+        (list) => list.task_title === updateModal?.task_title
+      );
+
+      // check if the list to update exists and is truthy
+      if (listToUpdate) {
+        // check if the category of the list to update matches the input category
+        if (updateModal?.category === inputValue.category) {
+          // update the properties of the list object
+          listToUpdate[0].task_title = inputValue.task_title;
+          listToUpdate[0].date_time = inputValue.date_time;
+
+          // store the modified sub-array in localStorage
+          localStorage.setItem(
+            updateModal?.category,
+            JSON.stringify(updatedLists[updateModal?.index])
+          );
+        } else {
+          // remove the updated item from the original array
+          let items = Lists[updateModal?.index]?.filter(
+            (i) => i?.task_title !== updateModal?.task_title
+          );
+
+          // store the modified sub-array in localStorage
+          localStorage.setItem(updateModal?.category, JSON.stringify(items));
+
+          // add the updated item to the new category in localStorage
+          let isCategoryexist = JSON.parse(
+            localStorage.getItem(inputValue.category)
+          );
+          if (isCategoryexist) {
+            isCategoryexist.push(inputValue);
+            localStorage.setItem(
+              inputValue.category,
+              JSON.stringify(isCategoryexist)
+            );
+          } else {
+            localStorage.setItem(
+              inputValue.category,
+              JSON.stringify([inputValue])
+            );
+          }
+        }
+      }
+    } else {
+      // add the input value to localStorage
+      let isCategoryexist = JSON.parse(
+        localStorage.getItem(inputValue.category)
+      );
+      if (isCategoryexist) {
+        isCategoryexist.push(inputValue);
+        localStorage.setItem(
+          inputValue.category,
+          JSON.stringify(isCategoryexist)
+        );
+      } else {
+        localStorage.setItem(inputValue.category, JSON.stringify([inputValue]));
+      }
+    }
+    window.location.reload(); // reload the page
+  };
+
+  // console.log(inputValue);
+
+  return (
+    <div className="z-10 w-screen h-full md:h-screen absolute top-0 left-0 flex items-center justify-center bg-black/70">
+      <form onSubmit={handleSubmit} className="relative form bg-neutral">
+        <button
+          onClick={() => {
+            Object.keys(updateModal).length !== 0 && window.location.reload();
+            setOpenModal(false);
+          }}
+          className="text-error absolute top-2 right-2 p-2"
+        >
+          <IoCloseSharp fontSize={25} />
+        </button>
+        <div className="flex">
+          <label>
+            <select
+              name="category"
+              required=""
+              className="select select-primary w-full max-w-xs"
+              value={inputValue.category}
+              onChange={handleChange}
+            >
+              <option value="">Pick one</option>
+              <option value="urgent-important">Urgent &amp; Important</option>
+              <option value="not-urgent-important">
+                Not Urgent &amp; Important
+              </option>
+              <option value="urgent-not-important">
+                Urgent &amp; Not Important
+              </option>
+              <option value="not-urgent-not-important">
+                Not Urgent &amp; Not Important
+              </option>
+            </select>
+          </label>
+
+          <label>
+            <input
+              required=""
+              name="task_title"
+              placeholder="Enter Task"
+              type="text"
+              value={inputValue.task_title}
+              className="input input-bordered input-primary w-full max-w-xs"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <label>
+          <input
+            required=""
+            name="date_time"
+            type="datetime-local"
+            value={inputValue.date_time}
+            className="input input-bordered input-primary w-full max-w-xs"
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit" className="btn btn-primary">
+          {updateModal?.update ? "update" : "submit"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,158 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Modal from "./Modal";
+import { MdOutlineDeleteOutline, MdOutlineMic } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
+import { BiEdit } from "react-icons/bi";
+
+const TodoList = () => {
+ 
+  const [OpenModal, setOpenModal] = useState(false);
+  const [Lists, setLists] = useState();
+  let categoryArray = [
+    "Urgent & Important",
+    "Not Urgent & Important",
+    "Urgent & Not Important",
+    "Not Urgent & Not Important"
+  ];
+  
+  useEffect(() => {
+    let StoredList = [
+      JSON.parse(localStorage.getItem("urgent-important")),
+      JSON.parse(localStorage.getItem("not-urgent-important")),
+      JSON.parse(localStorage.getItem("urgent-not-important")),
+      JSON.parse(localStorage.getItem("not-urgent-not-important"))
+    ];
+    setLists(StoredList);
+  }, []);
+
+  if (Lists) {
+    const now = new Date().toISOString().slice(0, 16);
+    let filterByDateDelete = Lists[1]?.filter((obj) => obj.date_time > now);
+    let filterByDateMove = Lists[1]?.filter((obj) => obj.date_time < now);
+    
+    localStorage.setItem(
+      "not-urgent-important",
+      JSON.stringify(filterByDateDelete)
+      );
+      let isCategoryexist = JSON.parse(localStorage.getItem("urgent-important"));
+      if (isCategoryexist) {
+        filterByDateMove.forEach((element) => {
+          element.category = "urgent-important";
+        isCategoryexist.push(element);
+      });
+      localStorage.setItem("urgent-important", JSON.stringify(isCategoryexist));
+    } else {
+      let newArray = [];
+      filterByDateMove.forEach((element) => {
+        element.category = "urgent-important";
+        newArray.push(element); // add object to array
+      });
+      localStorage.setItem("urgent-important", JSON.stringify(newArray)); // store updated array in localStorage
+    }
+    // console.log(filterByDateMove)
+  }
+  
+  const handleInputChange = (e, index, item) => {
+    const { checked } = e.target;
+    if (checked) {
+      let updatedLists = [...Lists]; // create a copy of the original array
+      const listToUpdate = updatedLists[index]?.filter(
+        // console.log(listToUpdate);
+        (list) => list.task_title === item.task_title
+        );
+        if (listToUpdate) {
+          listToUpdate[0].completed = true; // update the object's "completed" property
+          localStorage.setItem(
+            item.category,
+            JSON.stringify(updatedLists[index])
+            ); // store the modified sub-array in localStorage
+          }
+        }
+  };
+  
+  const [updateModal, setupdateModal] = useState();
+  
+  const handleEdit = (index, item) => {
+    setOpenModal(true);
+    item.update = true;
+    item.index = index;
+    // console.log(item);
+    setupdateModal(item);
+  };
+  
+  if (Lists) {
+    return (
+      <div className="w-screen bg-base-100 relative h-full md:h-screen z-0 flex flex-col items-center justify-center p-2">
+          <button
+            className="btn btn-primary mb-2"
+            onClick={() => setOpenModal(true)}
+            >
+            <IoMdAdd className="mr-2" fontSize={18} />
+            Add
+          </button>
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-2 w-[90%] h-[90%]">
+          {categoryArray.map((gridCategory, gridIndex) => (
+            <div key={gridIndex} className="border-2 rounded-lg text-center">
+              <h2 className="text-xl shadow-sm shadow-white p-1">
+                {gridCategory}
+              </h2>
+              <ul className="p-2">
+                {Lists[gridIndex]?.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center w-full border-b-2 border-primary p-2"
+                  >
+                    <div className="flex items-center">
+                      <input
+                        className="checkbox checkbox-primary"
+                        type="checkbox"
+                        checked={item?.completed}
+                        onChange={(e) => handleInputChange(e, gridIndex, item)}
+                      />
+                      <p className="text-lg font-light">{item?.task_title}</p>
+                      <p className="text-sm">{item?.date_time}</p>
+                    </div>
+                    <button
+                      className="p-1"
+                      onClick={() => {
+                        let items = Lists[gridIndex]?.filter(
+                          (i) => i?.task_title !== item?.task_title
+                        );
+                        localStorage.setItem(
+                          item?.category,
+                          JSON.stringify(items)
+                        );
+                        window.location.reload();
+                      }}
+                    >
+                      <MdOutlineDeleteOutline
+                        className="text-error"
+                        fontSize={25}
+                      />
+                    </button>
+                    <button
+                      className="p-1"
+                      onClick={() => handleEdit(gridIndex, item)}
+                    >
+                      <BiEdit className="text-secondary" fontSize={25} />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        {OpenModal && (
+          <Modal
+            setOpenModal={setOpenModal}
+            updateModal={updateModal}
+            Lists={Lists}
+          />
+        )}
+      </div>
+    );
+  }
+};
+
+export default TodoList;
